481429dcfa7ea0e95426b96b5b8fa3ac3bb6f7c8
MANASA TONDUR <102951222+CodeWith-Manasa@users.noreply.github.com>
2023-10-16 11:48:59 +0530

    Update RomanNumber.java

--- JAVA/RomanNumber.java
+++ JAVA/RomanNumber.java
@@ -1,9 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class RomanNumber {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Map<Character, Integer> romanToInteger = new HashMap<>();
         romanToInteger.put('I', 1);
         romanToInteger.put('V', 5);
@@ -16,7 +17,16 @@ public class RomanNumber {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a Roman numeral: ");
         String romanNumeral = scanner.nextLine().toUpperCase();
+
         
+        String pattern = "^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$";
+
+        if (!Pattern.matches(pattern, romanNumeral)) {
+            System.out.println("Invalid Roman numeral.");
+            scanner.close();
+            return;
+        }
+
         int result = 0;
         int prevValue = 0;
         for (int i = romanNumeral.length() - 1; i >= 0; i--) {
@@ -33,7 +43,5 @@ public class RomanNumber {
 
         System.out.println("The integer value of " + romanNumeral + " is: " + result);
         scanner.close();
-
     }
-    
 }
